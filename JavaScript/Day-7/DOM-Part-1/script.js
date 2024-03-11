81c751b2ed82330f777c3c460f8feb0121dde348
sudhanshu--77 <stripathi1721@gmail.com>
2024-03-12 01:48:48 +0530

    DOM Part - 1

--- JavaScript/Day-7/DOM-Part-1/script.js
+++ JavaScript/Day-7/DOM-Part-1/script.js
@@ -5,6 +5,11 @@ console.log(window);
 console.dir(document.body)
 
 // HTML ko javaScript ke andar access krneka tarika hai DOM.
+/* 4 Pillars of DOM :- 
+1. Selection of an Element
+2. Changing HTML
+3. Changing Css
+4. Event Listener   */
 
 
 // DOM Manipulation :- 
@@ -15,23 +20,23 @@ let heading = document.getElementById("heading"); //h1
 console.log(heading);
 
 
-/*2. Selecting with class <br>
-   document.getElementsByClassName("myClass") <br> -> return HTML collections, which is similar to array */
+/*2. Selecting with class 
+   document.getElementsByClassName("myClass")  -> return HTML collections, which is similar to array */
 let headings = document.getElementsByClassName("heading");
 console.dir(headings);
 console.log(headings);
 
-/*3. Selecting with tag <br>
+/*3. Selecting with tag 
    document.getElementsByTagName("p") */
 
 let parahs = document.getElementsByTagName("p");
 console.dir(parahs);
 
 /*4. Query Selector
-   document.querySelector("myID/myClass/tag") <br>
+   document.querySelector("myID/myClass/tag") 
    -> returns first element
 
-   document.querySelectorAll("myId/myClass/tag") <br>
+   document.querySelectorAll("myId/myClass/tag") 
    -> returns a NodeList */
 
 let firselements = document.querySelector(".myClass");
@@ -46,3 +51,32 @@ console.dir(allElements);
  * inner HTML : returns the plain text or HTML contents in the element
  * textContent : returns textual content even for hidden elements */
 
+// Difference between inner HTML and Text Content;
+
+var box = document.querySelector("#box");
+box.innerHTML = "<h1> Hello </h1>" ;
+
+
+var box = document.querySelector("#box");
+box.textContent = "<h1> Hello </h1>" ;
+
+
+// Event Listener
+
+var bulb = document.querySelector(".bulb1");
+var btn = document.querySelector("button");
+
+var flag = 0
+
+btn.addEventListener("click",function(){
+   if(flag == 0){
+      bulb.style.backgroundColor = "red"
+      console.log("Cliocked")
+      flag = 1
+   } else {
+      bulb.style.backgroundColor = "transparent"
+      console.log("Click Again")
+      flag = 0
+   }
+   
+});
